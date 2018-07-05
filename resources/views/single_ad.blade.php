be389061cacf4e5fefd9658f36f81dd181276009
Ivan Jovkovic <jokac14@gmail.com>
2018-07-05 12:01:26 +0200

    place bid button

--- resources/views/single_ad.blade.php
+++ resources/views/single_ad.blade.php
@@ -85,7 +85,7 @@
 
         <div class="container">
             <div class="row">
-                <div class="col-sm-7 col-lg-8">
+                <div class="col-sm-8">
 
                     @include('admin.flash_msg')
 
@@ -306,7 +306,7 @@
 
                 </div>
 
-                <div class="col-sm-5 col-lg-4">
+                <div class="col-sm-4">
                     <div class="sidebar-widget">
 
                         @if($ad->category_type == 'auction')
